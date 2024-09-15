a6e32a9402d9dbb6f5428ef0313d3537ea20edac
bluestorm <srijadutta0420@gmail.com>
2024-09-15 21:03:53 +0530

    updates

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,15 +8,16 @@ export default async function Header(){
     const client = createClient();
 
     const settings = await client.getSingle("settings");
+    
     return(
-        <section as ="header" className='transparent font-bold py-4 md:py-2 lg:py-2'>
+        <header className='transparent font-bold py-4 md:py-2 lg:py-2'>
             <div className="flex gap-20 items-center justify-between px-4 py-2 md:flex">
                 <Link href="/">
                     <Image src="/images/logo.png" alt="nyxNOVA" width={100} height={10} />
                 </Link>
                 <nav>
                     <ul className='flex text-lg text-yellow-200 hover:text-slate-200 font-body '>
-                        {settings.data.navigation.map(({link, label})=>(
+                        {settings.data.navigation.map(({link, label}) => (
                             <li key={label}>
                                 <PrismicNextLink field={link} className="p-3">
                                     {label}
@@ -25,8 +26,7 @@ export default async function Header(){
                         ))}
                     </ul>
                 </nav>
-                
             </div>
-        </section>
+        </header>
     )
 }
